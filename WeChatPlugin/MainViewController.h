cd69e4b880b2f76b56ece3cbaa90970d684306d9
yj.hou <houmanager@qq.com>
2017-09-08 11:00:10 +0800

    1111

---
+++ WeChatPlugin/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  WeChatPlugin
+//
+//  Created by YJHou on 2017/9/8.
+//  Copyright © 2017年 houmanager@hotmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ WeChatPlugin/MainViewController.m
@@ -0,0 +1,37 @@
+//
+//  MainViewController.m
+//  WeChatPlugin
+//
+//  Created by YJHou on 2017/9/8.
+//  Copyright © 2017年 houmanager@hotmail.com. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
